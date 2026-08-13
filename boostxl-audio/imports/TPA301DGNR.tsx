2135c0aa9a0e61eb03982d272943886894a526a7
Mustafa Mulla <mustafamulla765@gmail.com>
2026-08-13 12:34:26 +0530

    Fix imports for boostxl-audio

--- boostxl-audio/imports/TPA301DGNR.tsx
+++ boostxl-audio/imports/TPA301DGNR.tsx
@@ -1,4 +1,4 @@
-import type { ChipProps } from "@tscircuit/props"
+import type { ChipProps } from "@tscircuit/props";
 
 const pinLabels = {
   pin1: ["SHUTDOWN"],
@@ -9,27 +9,33 @@ const pinLabels = {
   pin6: ["VDD"],
   pin7: ["GND"],
   pin8: ["VO_NEG"],
-  pin9: ["EP"]
-} as const
+  pin9: ["EP"],
+} as const;
 
 const footprinterPinLabels = {
   ...pinLabels,
-  "pin9": [...pinLabels["pin9"], "thermalpad"],
-} as const
+  pin9: [...pinLabels["pin9"], "thermalpad"],
+} as const;
 
 export const TPA301DGNR = (props: ChipProps<typeof pinLabels>) => {
   return (
     <chip
       pinLabels={footprinterPinLabels}
       supplierPartNumbers={{
-  "jlcpcb": [
-    "C544668"
-  ]
-}}
+        jlcpcb: ["C544668"],
+      }}
       manufacturerPartNumber="TPA301DGNR"
       externallyConnectedPins={[["pin7", "pin9"]]}
       footprint="vssop8_thermalpad1.5mmx1.8mm_pw0.364mm_pl1.43mm_pin1location(leftside,bottom)"
+      cadModel={{
+        objUrl:
+          "https://modelcdn.tscircuit.com/easyeda_models/assets/C544668.obj?uuid=623c28a9e7234740a83ac7ee7c966295",
+        stepUrl:
+          "https://modelcdn.tscircuit.com/easyeda_models/assets/C544668.step?uuid=623c28a9e7234740a83ac7ee7c966295",
+        pcbRotationOffset: 0,
+        modelOriginPosition: { x: 0, y: 0.000012699999999199463, z: -0.6 },
+      }}
       {...props}
     />
-  )
-}
+  );
+};
